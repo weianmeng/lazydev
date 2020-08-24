fefcd3a560c04011ab68c898286dbe9fafabae90
anmeng.wei <weianmeng@live.com>
2020-08-24 09:39:43 +0800

    提交

--- src/lazyDev.Dapper/DapperProxy.cs
+++ src/lazyDev.Dapper/DapperProxy.cs
@@ -161,7 +161,6 @@ namespace lazyDev.Dapper
             return await Execute(x => x.QuerySingleAsync<T>(sql, param), sql, param);
         }
 
-
         public async Task<T> QuerySingleOrDefaultAsync<T>(
             string sql,
             object param = null)
