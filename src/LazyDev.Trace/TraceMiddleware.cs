4057113cbe495e649378530c1455be52d478369a
anmeng.wei <weianmeng@live.com>
2020-08-31 18:24:25 +0800

    提交

--- src/LazyDev.Trace/TraceMiddleware.cs
+++ src/LazyDev.Trace/TraceMiddleware.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
 using Microsoft.AspNetCore.Http;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
@@ -22,7 +25,7 @@ namespace LazyDev.Trace
 
             //设置头部信息 链路ID, 父亲追踪ParentTraceId,当前TraceId
             BuildHeader(context);
-           
+            var requestBodyString = await FormatRequestAsync(context.Request);
             //读取请求信息
             await _next(context);
             //读取响应信息
@@ -30,6 +33,17 @@ namespace LazyDev.Trace
             //写入日志
            _logger.LogTrace(LogLevel.Information,new TraceMessage()
            {
+               Request = new Request()
+               {
+                 Path = $"{context.Request.Path}{context.Request.QueryString.Value}",
+                 Method = context.Request.Method,
+                 Body = requestBodyString?.Length > 4096 ? $"{requestBodyString.Substring(0, 4096)}..." : requestBodyString,
+                 Headers = context.Request?.Headers.ToDictionary(s => s.Key, s => string.Join(",", s.Value.ToArray()))
+               },
+               Response = new Response()
+               {
+
+               },
                TimeSpan = sw.ElapsedMilliseconds
            });
         }
@@ -61,6 +75,34 @@ namespace LazyDev.Trace
                 context.Request.Headers.Add(TraceConst.TraceId, Guid.NewGuid().ToString());
             }
 
+        }
+
+        /// <summary>
+        /// 读取请求Body数据
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        private async Task<string> FormatRequestAsync(HttpRequest request)
+        {
+            try
+            {
+                if (!request.ContentLength.HasValue || request.ContentLength.Value <= 0) return "";
+
+                request.EnableBuffering();
+
+                using (var reader = new StreamReader(request.Body, Encoding.UTF8, false, 1024, true))
+                {
+                    var requestBody = await reader.ReadToEndAsync();
+                    request.Body.Position = 0;
+                    return requestBody;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "获取Request Body 错误");
+                throw;
+            }
         }
     }
 }
